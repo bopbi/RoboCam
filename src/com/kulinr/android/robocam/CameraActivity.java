d6337699281aaf28bacfc6ba4037773189679230
Bobby Prabowo <prabowo.bobby@gmail.com>
2012-12-07 05:36:00 +0700

    add preview code
    - add compatibility
    - using landscape

---
+++ src/com/kulinr/android/robocam/CameraActivity.java
@@ -0,0 +1,23 @@
+package com.kulinr.android.robocam;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CameraActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		CameraSurface cameraSurface = new CameraSurface(this);
+		setContentView(cameraSurface);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_camera, menu);
+		return true;
+	}
+
+}
